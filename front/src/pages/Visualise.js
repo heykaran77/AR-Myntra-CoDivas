a630ff9aa5201c061434658f6dc02e2a5ddb99db
aasmithadhani <aasmithadhani@gmail.com>
2024-07-15 18:04:51 +0530

    minute changes

--- front/src/pages/Visualise.js
+++ front/src/pages/Visualise.js
@@ -12,6 +12,7 @@ const Visualise = () => {
   const { responseImages,setResponseImages } = useContext(ImageContext);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [feedbackArray, setFeedbackArray] = useState([]);
+  const [search,setSearch] = useState(null);
 
   const handleClick = (feedback) => {
     if (currentIndex < responseImages.length) {
@@ -36,6 +37,25 @@ const Visualise = () => {
 
   const currentImage = responseImages[currentIndex];
 
+  const handleSearch = () =>
+  {
+    axios.post('http://localhost:8000/search', { search:search })
+        .then(response => {
+          console.log('Feedback submitted:', response.data);
+          setResponseImages(response.data.images); 
+          setFeedbackArray([]);
+          setCurrentIndex(0);
+        })
+        .catch(error => {
+          console.error('Error submitting feedback:', error);
+        });   
+  }
+
+  const handleChange = (e) =>
+  {
+      setSearch(e.target.value)
+  }
+
   return (
     <Grid container className='h-screen'>
       <Grid item xs={12}>
@@ -48,10 +68,12 @@ const Visualise = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search ..."
                 required
+                onChange={handleChange}
               />
             </div>
             <button
               type="submit"
+              onClick = {handleSearch}
               className="p-2.5 ms-6 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
