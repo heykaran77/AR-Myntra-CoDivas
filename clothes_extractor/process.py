90541fdb5566fcb309634b4ee933a152186d41b8
Kashish Gandhi <102237174+Kashish-G@users.noreply.github.com>
2024-07-29 22:01:27 +0530

    fixed for bottom wear extraction too

--- clothes_extractor/process.py
+++ clothes_extractor/process.py
@@ -8,7 +8,6 @@ import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 from collections import OrderedDict
-
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 
@@ -73,23 +72,42 @@ def apply_transform(img):
     transform_rgb = transforms.Compose(transforms_list)
     return transform_rgb(img)
 
-def generate_mask(input_image, net, device='cpu'):
+def generate_mask(input_image, net, palette, device='cpu'):
     img = input_image
     img_size = img.size
     img = img.resize((768, 768), Image.BICUBIC)
     image_tensor = apply_transform(img)
     image_tensor = torch.unsqueeze(image_tensor, 0)
+
     with torch.no_grad():
         output_tensor = net(image_tensor.to(device))
         output_tensor = F.log_softmax(output_tensor[0], dim=1)
         output_tensor = torch.max(output_tensor, dim=1, keepdim=True)[1]
         output_tensor = torch.squeeze(output_tensor, dim=0)
         output_arr = output_tensor.cpu().numpy()
-    alpha_mask = (output_arr[0] == 1).astype(np.uint8) * 255
-    alpha_mask = Image.fromarray(alpha_mask, mode='L')
-    alpha_mask = alpha_mask.resize(img_size, Image.BICUBIC)
-    return alpha_mask
 
+    classes_to_save = []
+
+    for cls in range(1, 4):
+        if np.any(output_arr == cls):
+            classes_to_save.append(cls)
+
+    alpha_masks = []
+    for cls in classes_to_save:
+        alpha_mask = (output_arr == cls).astype(np.uint8) * 255
+        alpha_mask = alpha_mask[0]
+        alpha_masks.append(alpha_mask)
+
+    if alpha_masks:
+        combined_alpha_mask = np.max(alpha_masks, axis=0)
+    else:
+        combined_alpha_mask = np.zeros_like(output_arr[0], dtype=np.uint8)
+    combined_alpha_mask = Image.fromarray(combined_alpha_mask, mode='L')
+    combined_alpha_mask = combined_alpha_mask.resize(img_size, Image.BICUBIC)
+    cloth_seg = Image.fromarray(output_arr[0].astype(np.uint8), mode='P')
+    cloth_seg.putpalette(palette)
+    cloth_seg = cloth_seg.resize(img_size, Image.BICUBIC)
+    return combined_alpha_mask, cloth_seg
 
 
 def load_seg_model(checkpoint_path, device='cpu'):
@@ -100,14 +118,16 @@ def load_seg_model(checkpoint_path, device='cpu'):
     return net
 
 
-def save_transparent_image(image_path, alpha_mask_cv, final_output_with_white_bg_path):
+def save_transparent_image(image_path, alpha_mask, final_output_with_white_bg_path):
     original_image = cv2.imread(image_path)
-    combined_alpha_mask = alpha_mask_cv 
+    alpha_mask = cv2.resize(alpha_mask, (original_image.shape[1], original_image.shape[0]))
+
     if len(original_image.shape) == 2: 
         original_image = cv2.cvtColor(original_image, cv2.COLOR_GRAY2BGRA)
-    elif len(original_image.shape) == 3 and original_image.shape[2] == 3: 
+    elif len(original_image.shape) == 3 and original_image.shape[2] == 3:
         original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2BGRA)
-    transparent_image = cv2.merge([original_image[:, :, 0], original_image[:, :, 1], original_image[:, :, 2], combined_alpha_mask])
+
+    transparent_image = cv2.merge([original_image[:, :, 0], original_image[:, :, 1], original_image[:, :, 2], alpha_mask])
     if transparent_image.shape[2] == 4:
         b, g, r, a = cv2.split(transparent_image)
         white_background = np.ones_like(transparent_image[:, :, :3]) * 255
@@ -117,16 +137,18 @@ def save_transparent_image(image_path, alpha_mask_cv, final_output_with_white_bg
             white_background[:, :, c] = (alpha * transparent_image[:, :, c] +
                                           alpha_inv * white_background[:, :, c])
         cv2.imwrite(final_output_with_white_bg_path, white_background)
-
+    else:
+        cv2.imwrite(final_output_with_white_bg_path, transparent_image)
 
 def main(args):
     final_output_with_white_bg_path = './output/final_image_with_white_bg.png'
     device = 'cuda:0' if args.cuda else 'cpu'
     model = load_seg_model(args.checkpoint_path, device=device)
+    palette = get_palette(4)
     img = Image.open(args.image).convert('RGB')
-    alpha_mask = generate_mask(img, model, device)
-    alpha_mask_cv = np.array(alpha_mask)
-    save_transparent_image(args.image, alpha_mask_cv, final_output_with_white_bg_path)
+    combined_alpha_mask, cloth_seg_image = generate_mask(img, model, palette)
+    alpha_mask = np.array(combined_alpha_mask)
+    save_transparent_image(args.image, alpha_mask, final_output_with_white_bg_path)
 
 
 if __name__ == '__main__':
